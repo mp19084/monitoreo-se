c47841b52ca5669f59664e13433d70b517540f99
VM17015 <VM17015@ues.edu.sv>
2020-11-11 03:47:28 -0600

    Explicacion de Asignacion2

--- database/seeds/PruebaSeeder.php
+++ database/seeds/PruebaSeeder.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Autor;
+use App\Componente;
+use App\Libro;
+use App\SistemaEmbebido;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class PruebaSeeder extends Seeder
@@ -12,5 +17,46 @@ class PruebaSeeder extends Seeder
     public function run()
     {
         //
+        Autor::create([
+            'nombre' => 'Rafael Viscarra',
+            'edad' => 23
+        ]);
+
+        Autor::create([
+            'nombre' => 'Juan Antonio',
+            'edad' => 25
+        ]);
+
+        Libro::create([
+            'titulo' => 'Primer libro',
+            'precio' => 25.50,
+            'disponible' => false,
+            'autor_id' => 1,
+            'publicado' => Carbon::now(),
+        ]);
+
+        Libro::create([
+            'titulo' => 'Segundo Libro',
+            'precio' => 30.00,
+            'disponible' => true,
+            'autor_id' => 1,
+            'publicado' => Carbon::now(),
+        ]);
+
+        Libro::create([
+            'titulo' => 'Tercer libro',
+            'precio' => 15.75,
+            'disponible' => true,
+            'autor_id' => 2,
+            'publicado' => Carbon::now(),
+        ]);
+
+        Libro::create([
+            'titulo' => 'Cuarto libro',
+            'precio' => 10.20,
+            'disponible' => true,
+            'autor_id' => 2,
+            'publicado' => Carbon::now(),
+        ]);
     }
 }
