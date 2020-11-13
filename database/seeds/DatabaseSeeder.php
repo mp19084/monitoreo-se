1c943a2abff0fa931e0391a2ee51dc2c01573e80
Guillermo Amaya <al13013@ues.edu.sv>
2020-11-13 15:46:07 -0600

    Seeder VM17015Seeder agregado a DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        $this->call(VM17015Seeder::class);
         $this->call(AL13013Seeder::class);
     }
 }
