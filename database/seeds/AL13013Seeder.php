8bd2d44a552a3f3db9f835f34d361d91f57836bc
Guillermo Amaya <al13013@ues.edu.sv>
2020-11-12 22:02:25 -0600

    Seeder agregados y modificados

--- database/seeds/AL13013Seeder.php
+++ database/seeds/AL13013Seeder.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\SistemaEmbebido;
+use App\TipoDato;
+use App\Unidad;
+use App\Medicion;
+use App\Componente;
 
 class AL13013Seeder extends Seeder
 {
@@ -12,5 +17,117 @@ class AL13013Seeder extends Seeder
     public function run()
     {
         //
+        
+
+        SistemaEmbebido::create([
+            'user_id' => 1,
+            'nombre' => 'Sistema 1',
+            'activo' => true
+        ]);
+
+        SistemaEmbebido::create([
+            'user_id' => 1,
+            'nombre' => 'Sistema 2',
+            'activo' => false
+        ]);
+
+        TipoDato::create([
+            'nombre' => 'Entero'
+        ]);
+
+        TipoDato::create([
+            'nombre' => 'Decimal'
+        ]);
+
+        TipoDato::create([
+            'nombre' => 'Cadena de Texto'
+        ]);
+
+        Unidad::create([
+            'nombre' => 'Metro',
+            'abreviacion' => 'm'
+        ]);
+
+        Unidad::create([
+            'nombre' => 'Kilogramo',
+            'abreviacion' => 'kg'
+        ]);
+
+        Unidad::create([
+            'nombre' => 'Segundo',
+            'abreviacion' => 's'
+        ]);
+
+        Componente::create([
+            'sistema_embebido_id'=>1,
+            'tipo_dato_id'=>1,
+            'unidad_id'=>1,
+            'nombre'=>'cuarto sistema'
+
+        ]);
+
+        Componente::create([
+            'sistema_embebido_id'=>1,
+            'tipo_dato_id'=>2,
+            'unidad_id'=>3,
+            'nombre'=>'primer componente'
+
+        ]);
+
+        Componente::create([
+            'sistema_embebido_id'=>2,
+            'tipo_dato_id'=>3,
+            'unidad_id'=>2,
+            'nombre'=>'segundo componente'
+
+        ]);
+
+        Componente::create([
+            'sistema_embebido_id'=>2,
+            'tipo_dato_id'=>1,
+            'unidad_id'=>3,
+            'nombre'=>'tercer componente'
+
+        ]);
+
+        Medicion::create([
+            'componente_id'=>1,
+            'valor'=>'muy bajo'
+        ]);
+
+        Medicion::create([
+            'componente_id'=>2,
+            'valor'=>'bajo'
+        ]);
+
+        Medicion::create([
+            'componente_id'=>3,
+            'valor'=>'alto'
+        ]);
+
+        Medicion::create([
+            'componente_id'=>4,
+            'valor'=>'muy alto'
+        ]);
+
+        Medicion::create([
+            'componente_id'=>1,
+            'valor'=>'bajo'
+        ]);
+
+        Medicion::create([
+            'componente_id'=>2,
+            'valor'=>'medio'
+        ]);
+
+        Medicion::create([
+            'componente_id'=>3,
+            'valor'=>'alto'
+        ]);
+
+        Medicion::create([
+            'componente_id'=>4,
+            'valor'=>'muy alto'
+        ]);
     }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,8 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
-        $this->call(VM17015Seeder::class);
-        $this->call(PruebaSeeder::class);
-        $this->call(MP19084Seeder::class);
+        $this->call(AL13013Seeder::class);
     }
 }
